09f3bce576dd03ee317bb926747a32d2969dc59e
roman.manko <romanmanco@gmail.com>
2021-06-05 12:32:56 +0300

    4.2. Тернарное сравнение [#189 #164809]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,10 +1,9 @@
 package ru.job4j.condition;
 
 public class Max {
+    @SuppressWarnings("checkstyle:InnerAssignment")
     public static int max(int left, int right) {
-        boolean condition = left > right;
-        int result = condition ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
