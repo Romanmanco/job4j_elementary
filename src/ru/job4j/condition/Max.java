151e8875226ca4966f5ab8ded4e4313d100c1c89
roman.manko <romanmanco@gmail.com>
2021-06-05 19:48:55 +0300

    4.2. Тернарное сравнение [#189 #164809]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,13 +1,12 @@
 package ru.job4j.condition;
 
 public class Max {
-    @SuppressWarnings("checkstyle:InnerAssignment")
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
 
     public static void main(String[] args) {
-        int rsl = Max.max(2143235, 35654);
+        int rsl = Max.max(767868, 35654);
         System.out.println(rsl);
     }
 }
