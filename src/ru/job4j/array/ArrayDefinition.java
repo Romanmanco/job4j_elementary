6034bab53784f55ca04c23fc6acb18098f7ebdc3
roman.manko <romanmanco@gmail.com>
2021-06-17 15:59:34 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,12 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Roman Manko";
+        names[1] = "Karina Cool";
+        names[2] = "Denis Sedikov";
+        names[3] = "Ksenya Bondarenko";
 
-        System.out.println("Размер массива равен: " + ages.length);
-        System.out.println("Размер массива равен: " + surnames.length);
-        System.out.println("Размер массива равен: " + prices.length);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
