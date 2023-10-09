f77acfe921d9f2ced43e0113a0c774b9e6c137d7
Romanmanco <romanmanco@gmail.com>
2023-10-09 11:05:03 +0300

    4. Аттестация [#504878 #450304]

--- src/main/java/ru/job4j/hmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hmap/AnalyzeByMap.java
@@ -4,56 +4,32 @@ import java.util.*;
 
 public class AnalyzeByMap {
     public static double averageScore(List<Pupil> pupils) {
-        Map<String, Double> subjectScoreMap = new HashMap<>();
-        Map<String, Integer> subjectCountMap = new HashMap<>();
-
-        for (Pupil pupil : pupils) {
-            for (Subject subject : pupil.subjects()) {
-                String subjectName = subject.name();
-                double score = subject.score();
-
-                subjectScoreMap.merge(subjectName, score, Double::sum);
-                subjectCountMap.merge(subjectName, 1, Integer::sum);
-            }
-        }
-
         double totalScore = 0.0;
         int totalCount = 0;
 
-        for (Map.Entry<String, Double> entry : subjectScoreMap.entrySet()) {
-            totalScore += entry.getValue();
-        }
-
-        for (Map.Entry<String, Integer> entry : subjectCountMap.entrySet()) {
-            totalCount += entry.getValue();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                totalScore += subject.score();
+                totalCount++;
+            }
         }
 
         return totalCount > 0 ? totalScore / totalCount : 0.0;
     }
 
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
-        Map<String, Double> pupilScoreMap = new HashMap<>();
-        Map<String, Integer> pupilCountMap = new HashMap<>();
+        List<Label> result = new ArrayList<>();
 
         for (Pupil pupil : pupils) {
             double sum = 0.0;
-            int count = 0;
+
             for (Subject subject : pupil.subjects()) {
                 sum += subject.score();
-                count++;
             }
-            double averageScore = count > 0 ? sum / count : 0.0;
 
-            pupilScoreMap.merge(pupil.name(), averageScore, Double::sum);
-            pupilCountMap.merge(pupil.name(), 1, Integer::sum);
+            double averageScore = pupil.subjects().isEmpty() ? 0.0 : sum / pupil.subjects().size();
+            result.add(new Label(pupil.name(), averageScore));
         }
-
-        List<Label> result = new ArrayList<>();
-        pupilScoreMap.forEach((pupilName, totalScore) -> {
-            int count = pupilCountMap.getOrDefault(pupilName, 0);
-            double averageScore = count > 0 ? totalScore / count : 0.0;
-            result.add(new Label(pupilName, averageScore));
-        });
 
         return result;
     }
@@ -83,27 +59,21 @@ public class AnalyzeByMap {
     }
 
     public static Label bestStudent(List<Pupil> pupils) {
-        Map<String, Double> pupilScoreMap = new HashMap<>();
-
-        for (Pupil pupil : pupils) {
-            double sum = 0.0;
-            for (Subject subject : pupil.subjects()) {
-                sum += subject.score();
-            }
-            double averageScore = sum;
-
-            pupilScoreMap.merge(pupil.name(), averageScore, Double::sum);
-        }
-
         String bestStudentName = "";
         double maxAverageScore = 0.0;
 
-        for (Map.Entry<String, Double> entry : pupilScoreMap.entrySet()) {
-            String studentName = entry.getKey();
-            double averageScore = entry.getValue();
+        for (Pupil pupil : pupils) {
+            double sum = 0.0;
+
+            for (Subject subject : pupil.subjects()) {
+                sum += subject.score();
+            }
+
+            double averageScore = sum;
+
             if (averageScore > maxAverageScore) {
                 maxAverageScore = averageScore;
-                bestStudentName = studentName;
+                bestStudentName = pupil.name();
             }
         }
 
